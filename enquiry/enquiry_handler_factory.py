9c16f454265e65bfbcc446b6378cebe0864fa5ab
Pradip Dharam <39373514+pradipdharam@users.noreply.github.com>
2024-06-15 12:46:42 +0530

    Created EnquiryHandlerFactory & implemented HandleEnquiryApi for demo.

---
+++ enquiry/enquiry_handler_factory.py
@@ -0,0 +1,20 @@
+from enquiry.academic_enquiry_handler import AcademicEnquiryHandler
+from enquiry.enquiry_handler import EnquiryHandler
+from enquiry.idle_handler import ProjectEnquiryHandler
+from enquiry.log_handler import LogHandler
+from enquiry.project_enquiry_handler import IdleHandler
+from enquiry.subscription_enquiry_handler import SubscriptionEnquiryHandler
+
+
+class EnquiryHandlerFactory:
+    def __new__(cls, *args, **kwargs):
+        return None
+
+    @staticmethod
+    def get_enquiry_handler() -> EnquiryHandler:
+        return LogHandler(
+            AcademicEnquiryHandler(
+                ProjectEnquiryHandler(
+                    SubscriptionEnquiryHandler(IdleHandler())
+                )
+            ))
--- handle_enquiry_api.py
+++ handle_enquiry_api.py
@@ -1,2 +1,13 @@
+from enquiry.enquiry_handler_factory import EnquiryHandlerFactory
+
+
 class HandleEnquiryApi:
-    pass
+    def handle_enquiry(self, enquiry: str):
+        EnquiryHandlerFactory.get_enquiry_handler().handle(enquiry)
+        return None
+
+
+if __name__ == "__main__":
+    EnquiryHandlerFactory.get_enquiry_handler().handle(
+        "I want to take 1 month Upgrade"
+    )
