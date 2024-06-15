0bc913ef953e2087bb1104ad61d81dac53b87ba6
Pradip Dharam <39373514+pradipdharam@users.noreply.github.com>
2024-06-15 12:53:21 +0530

    Corrected the incorrect file names. Optimized the imports.

--- enquiry/idle_handler.py
+++ enquiry/idle_handler.py
@@ -1,17 +1,8 @@
-from typing import Final
-
 from data.enquiry_type import EnquiryType
 from enquiry.enquiry_handler import EnquiryHandler
 
 
-class ProjectEnquiryHandler(EnquiryHandler):
-    __next_handler: Final[EnquiryHandler]
-
-    def __init__(self, next_handler: EnquiryHandler):
-        self.__next_handler = next_handler
-
+class IdleHandler(EnquiryHandler):
     def handle(self, enquiry: str) -> EnquiryType:
-        print("Inside ProjectEnquiryHandler")
-        if "Project" in enquiry:
-            return EnquiryType.PROJECT
-        return self.__next_handler.handle(enquiry)
+        print("Inside IdleHandler")
+        return EnquiryType.UNKNOWN
--- enquiry/project_enquiry_handler.py
+++
@@ -1,8 +0,0 @@
-from data.enquiry_type import EnquiryType
-from enquiry.enquiry_handler import EnquiryHandler
-
-
-class IdleHandler(EnquiryHandler):
-    def handle(self, enquiry: str) -> EnquiryType:
-        print("Inside IdleHandler")
-        return EnquiryType.UNKNOWN
