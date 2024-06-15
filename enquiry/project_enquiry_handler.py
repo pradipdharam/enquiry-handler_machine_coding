cb069c7100622a6102bb4cf40ad83addd0336177
Pradip Dharam <39373514+pradipdharam@users.noreply.github.com>
2024-06-15 12:43:21 +0530

    Concretion IdleHandler does not have member __next_handler, since it's a last node in chain or linked list, so does not need constructor.

--- enquiry/project_enquiry_handler.py
+++ enquiry/project_enquiry_handler.py
@@ -3,9 +3,6 @@ from enquiry.enquiry_handler import EnquiryHandler
 
 
 class IdleHandler(EnquiryHandler):
-    def __init__(self, next_handler: EnquiryHandler):
-        self.__next_handler = next_handler
-
     def handle(self, enquiry: str) -> EnquiryType:
         print("Inside IdleHandler")
         return EnquiryType.UNKNOWN
